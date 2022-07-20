ca262de5fab11bfff4add9a4077aaced05d1e0ab
amishasubedi <amisha.subedi1@gmail.com>
2022-07-19 22:41:39 -0500

    redirected and extracted params

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
@@ -7,11 +7,15 @@ import QuoteDetail from "./pages/QuoteDetail";
 function App() {
   return (
     <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+
       <Route path="/quotes" exact>
         <AllQuotes />
       </Route>
 
-      <Route path="./quotes/:quotesId">
+      <Route path="/quotes/:quoteId">
         <QuoteDetail />
       </Route>
 
