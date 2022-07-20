b82672cb9864205c60aad5822e940491bfad7add
amishasubedi <amisha.subedi1@gmail.com>
2022-07-19 23:06:07 -0500

    Added a Layout wrapper component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
@@ -6,23 +7,25 @@ import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
 
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
 
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
 
-      <Route path="/new-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
