58413bd6a69e80c73ad1a02c6c668c7688d03dde
amishasubedi <amisha.subedi1@gmail.com>
2022-07-20 00:56:19 -0500

    implemented programmatic navigation with useHistory hook

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,14 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  // useHistory hook allows access to the history instance, and redirects user to another page
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push("/quotes"); //navigate a way to quotes
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
