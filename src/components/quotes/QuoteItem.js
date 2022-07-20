4279ca75bffc9c0f1f867af34b6ccd72a6a0e51a
amishasubedi <amisha.subedi1@gmail.com>
2022-07-20 00:37:41 -0500

    output details of the quotes

--- src/components/quotes/QuoteItem.js
+++ src/components/quotes/QuoteItem.js
@@ -1,4 +1,5 @@
-import classes from './QuoteItem.module.css';
+import classes from "./QuoteItem.module.css";
+import { Link } from "react-router-dom";
 
 const QuoteItem = (props) => {
   return (
@@ -9,9 +10,9 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <a className='btn'>
-        View Fullscreen
-      </a>
+      <Link className="btn" to={`/quotes/${props.id}`}>
+        View Details
+      </Link>
     </li>
   );
 };
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,37 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Amisha",
+    text: "manche chinni agi badhni",
+  },
+
+  {
+    id: "q2",
+    author: "ams",
+    text: "react sikdai",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  // identify quote
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  // check if quote is undefined
+  if (!quote) {
+    return <p>No quote found</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
