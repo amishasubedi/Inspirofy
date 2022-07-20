b7db3fe6bb08fc7680b090fded13e9a54cd60241
amishasubedi <amisha.subedi1@gmail.com>
2022-07-19 22:48:42 -0500

    worked with nested routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
   const params = useParams();
@@ -8,6 +9,9 @@ const QuoteDetail = () => {
     <Fragment>
       <h1>Quote Detail Page</h1>
       <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
