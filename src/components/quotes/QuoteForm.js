f7ad4152a2acf72f05b1f88f6d6150ca0be5a0e3
amishasubedi <amisha.subedi1@gmail.com>
2022-07-21 02:15:54 -0500

    navigated away from page with prompt component

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,10 +1,13 @@
-import { useRef } from "react";
+import { Fragment } from "react";
+import { useRef, useState } from "react";
+import { Prompt } from "react-router-dom";
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
+  const [isEntering, setIsEntering] = useState(false);
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -19,14 +22,27 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+  }
+
+  const formFocusHandler = () => {
+    setIsEntering(true);
+  }
+
   return (
+    <Fragment>
+      {/* prompt is used before navigating away from a page */}
+      <Prompt when = {isEntering} message = {(location) => 'Are you sure you want to leave? All your entered data will be lost'}/>
     <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+      <form onFocus = {} className={classes.form} onSubmit={submitFormHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
           </div>
         )}
+
+
 
         <div className={classes.control}>
           <label htmlFor="author">Author</label>
@@ -37,10 +53,11 @@ const QuoteForm = (props) => {
           <textarea id="text" rows="5" ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className="btn">Add Quote</button>
+          <button onClick = {finishEnteringHandler} className="btn">Add Quote</button>
         </div>
       </form>
     </Card>
+    </Fragment>
   );
 };
 
