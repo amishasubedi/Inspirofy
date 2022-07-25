1ca57a68418db40f00764e41c2cc46044b03c696
amishasubedi <amisha.subedi1@gmail.com>
2022-07-24 23:45:23 -0500

    sent quote data via http

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,21 +1,58 @@
+import { useEffect } from "react";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
 import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
 
-const DUMMY_QUOTES = [
-  {
-    id: "q1",
-    author: "Amisha",
-    text: "manche chinni agi badhni",
-  },
+// const DUMMY_QUOTES = [
+//   {
+//     id: "q1",
+//     author: "Amisha",
+//     text: "manche chinni agi badhni",
+//   },
 
-  {
-    id: "q2",
-    author: "ams",
-    text: "react sikdai",
-  },
-];
+//   {
+//     id: "q2",
+//     author: "ams",
+//     text: "react sikdai",
+//   },
+// ];
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  // extract data
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true); // make sure status is pending from the start
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  // check for loading
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  // check for error
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  // not loading, no error but have no quotes
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  // no error, not loading, and have quotes
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,17 +1,28 @@
 import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
+
   // useHistory hook allows access to the history instance, and redirects user to another page
   const history = useHistory();
 
-  const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes"); // navigate a way to quotes
+    }
+  }, [status, history]);
 
-    history.push("/quotes"); //navigate a way to quotes
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
