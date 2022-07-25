61fc445450240b3a011ea3d133267d305daacdc4
amishasubedi <amisha.subedi1@gmail.com>
2022-07-25 15:41:08 -0500

    deployed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import LoadingSpinner from "./components/ui/LoadingSpinner";
 
-// lazy loadinggi
+// lazy loading
 const NewQuote = React.lazy(() => import("./pages/NewQuote"));
 const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
 const NotFound = React.lazy(() => import("./pages/NotFound"));
