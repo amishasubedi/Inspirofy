6b8637e374605b37ac953e24585e041991d4283d
amishasubedi <amisha.subedi1@gmail.com>
2022-07-24 23:52:33 -0500

    fetched the quote data

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,8 +1,11 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 import Comments from "../components/comments/Comments";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const DUMMY_QUOTES = [
   {
@@ -21,6 +24,39 @@ const DUMMY_QUOTES = [
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
+
+  const { quoteId } = params; // extract quote id from params
+
+  // destructured elements
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest, quoteId]);
+
+  // check for loading
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  // check for error
+  if (error) {
+    <p className="centered">{error}</p>;
+  }
+
+  // if loaded quote is empty
+  if (!loadedQuotes.text) {
+    return <p>No Quote found</p>;
+  }
 
   // identify quote
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
